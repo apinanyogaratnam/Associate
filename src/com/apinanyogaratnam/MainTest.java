d98e661f5588c36b3a86fa95fe81cf1cb2068d8f
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-06-04 12:27:03 -0400

    created new test companyInAllComaniesTest() and userInAllUsersTest()

--- src/com/apinanyogaratnam/MainTest.java
+++ src/com/apinanyogaratnam/MainTest.java
@@ -12,10 +12,10 @@ class MainTest {
     public void duplicateUsernameTest() {
         LinkedList<User> allUsers = new LinkedList<>();
 
-        // creates new users and companies
+        // create new user
         User apinan = mainMethod.createNewUser("Apinan", "Yogaratnam", "apinanyogaratnam", allUsers);
 
-        // creates null
+        // create duplicate user
         User apinanCopy = mainMethod.createNewUser("Apinan", "Yogaratnam", "apinanyogaratnam", allUsers);
 
         assertEquals(apinanCopy, null);
@@ -25,13 +25,51 @@ class MainTest {
     public void duplicateCompanyTest() {
         LinkedList<Company> allCompanies = new LinkedList<>();
 
-        // creates new users and companies
+        // creates new companies
         Company timhortons = mainMethod.createNewCompany("Tim Hortons", allCompanies);
         Company mcdonald = mainMethod.createNewCompany("McDonald's", allCompanies);
 
-        // creates null
+        // create duplicate company
         Company timhortonsCopy = mainMethod.createNewCompany("Tim Hortons", allCompanies);
 
         assertEquals(timhortonsCopy, null);
+    }
+
+    @Test
+    public void userInAllUsersTest() {
+        LinkedList<User> allUsers = new LinkedList<>();
+
+        // create new user
+        User apinan = mainMethod.createNewUser("Apinan", "Yogaratnam", "apinanyogaratnam", allUsers);
+
+        // search through allUsers for new user
+        boolean foundUser = false;
+        for (User user : allUsers) {
+            if (user.username.equals(apinan.username)) {
+                foundUser = true;
+                break;
+            }
+        }
+
+        assertEquals(foundUser, true);
+    }
+
+    @Test
+    public void companyInAllCompaniesTest() {
+        LinkedList<Company> allCompanies = new LinkedList<>();
+
+        // create new company
+        Company mcdonald = mainMethod.createNewCompany("McDonald's", allCompanies);
+
+        // search through allCompanies for new company
+        boolean foundCompany = false;
+        for (Company company : allCompanies) {
+            if (company.name.equals(mcdonald.name)) {
+                foundCompany = true;
+                break;
+            }
+        }
+
+        assertEquals(foundCompany, true);
     }
 }
