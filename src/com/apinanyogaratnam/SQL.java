6f52457d7f1e6b94a28b50d8cfb7ebf8bc0c286d
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-06-05 23:37:08 -0400

    removed addClass and addUser to db since combined into one function

--- src/com/apinanyogaratnam/SQL.java
+++ src/com/apinanyogaratnam/SQL.java
@@ -9,54 +9,6 @@ public class SQL {
     private static Secrets secrets = new Secrets();
     private static Print printClass = new Print();
 
-    public void addUserToDB(User user) {
-        try {
-            // get a connection to database
-            Connection connection = DriverManager.getConnection(secrets.url, secrets.username, secrets.password);
-
-            // create a statement
-            Statement statement = connection.createStatement();
-
-            // data to insert into database
-            String query = String.format("INSERT INTO users (first_name, last_name, username, friends) VALUES " +
-                    "(\"%s\", \"%s\", \"%s\", \"%s\");", user.firstName, user.lastName, user.username, "{}");
-
-            // insert data into database
-            statement.executeUpdate(query);
-
-            // close connection to server
-            connection.close();
-        } catch(SQLIntegrityConstraintViolationException e) {
-            printClass.print("object already exists in db.");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static void addCompanyToDB(Company company) {
-        try {
-            // get a connection to database
-            Connection connection = DriverManager.getConnection(secrets.url, secrets.username, secrets.password);
-
-            // create a statement
-            Statement statement = connection.createStatement();
-
-            // data to insert into database
-            String query = String.format("INSERT INTO companies (name, network_list, followers_list) VALUES " +
-                    "(\"%s\", \"%s\", \"%s\");", company.name, "{}", "{}");
-
-            // insert data into database
-            statement.executeUpdate(query);
-
-            // close connection to server
-            connection.close();
-        } catch(SQLIntegrityConstraintViolationException e) {
-            printClass.print("object already exists in db.");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
     public void addObjectToDB(Object obj) {
         String query = "";
         String errorMessage = "";
