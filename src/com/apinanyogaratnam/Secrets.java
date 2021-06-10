0de4eed9030b15734284a9a088024dc6a1b44a80
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-06-10 10:58:12 -0400

    db user and pass expired so unignoring secrets file

---
+++ src/com/apinanyogaratnam/Secrets.java
@@ -0,0 +1,19 @@
+package com.apinanyogaratnam;
+
+public class Secrets {
+    private final String url = "jdbc:mysql://sql5.freemysqlhosting.net:3306/sql5416936";
+    private final String username = "sql5416936";
+    private final String password = "6RPSZwqWzY";
+
+    public String getUrl() {
+        return this.url;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+}
