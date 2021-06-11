02220b9e8485dd6bdd48806432a2fd8341be3168
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-06-11 13:01:06 -0400

    created Utils unit test file

---
+++ src/com/apinanyogaratnam/UtilsTest.java
@@ -0,0 +1,20 @@
+package com.apinanyogaratnam;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UtilsTest {
+
+    @Test
+    void removeCurlyBraces() {
+    }
+
+    @Test
+    void splitCommas() {
+    }
+
+    @Test
+    void indexList() {
+    }
+}
