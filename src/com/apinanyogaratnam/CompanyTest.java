3466081dde982519ad648c0430ea381dfbaa0f26
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-06-12 13:30:58 -0400

    completed getnametest and getnetworkslisttest

--- src/com/apinanyogaratnam/CompanyTest.java
+++ src/com/apinanyogaratnam/CompanyTest.java
@@ -2,16 +2,34 @@ package com.apinanyogaratnam;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.LinkedList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CompanyTest {
 
     @Test
     void getName() {
+        LinkedList<Company> allCompanies = new LinkedList<>();
+
+        Company mcd = Main.createNewCompany("McDonald's", allCompanies, false);
+
+        // check if correct name is received
+        assertEquals("McDonald''s", mcd.getName());
     }
 
     @Test
     void getNetworksList() {
+        LinkedList<Company> allCompanies = new LinkedList<>();
+
+        Company mcd = Main.createNewCompany("McDonald's", allCompanies, false);
+        Company tims = Main.createNewCompany("Tims Hortons", allCompanies, false);
+
+        mcd.addNetwork(tims, allCompanies, false);
+
+        // check if tims is in networksList
+        assertEquals(1, mcd.getNetworksList().size());
+        assertTrue(mcd.getNetworksList().contains(tims));
     }
 
     @Test
