b209b0d5c38fbf5fb83c74d42c93e67aba348cba
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-06-12 15:51:49 -0400

    added comments for print class

--- src/com/apinanyogaratnam/Print.java
+++ src/com/apinanyogaratnam/Print.java
@@ -3,6 +3,7 @@ package com.apinanyogaratnam;
 import java.util.LinkedList;
 
 public class Print {
+    // using method overloading to use same method name with different parameters
     public static void print(String stringToPrint) {
         System.out.println(stringToPrint);
     }
@@ -29,10 +30,15 @@ public class Print {
     }
 
     public static void print(LinkedList<?> listOfObjects) {
+        // using generic objects to determine which object was passed through parameter
         for (Object obj : listOfObjects) {
+            // user object
             if (obj instanceof User) print("First name: " + ((User) obj).getFirstName() + ", Last name: "
                     + ((User) obj).getLastName() + ", Username: " + ((User) obj).getUsername());
+            // company object
             else if (obj instanceof Company) print("Company name: " + ((Company) obj).getName());
+
+            // object doesnt exist here
         }
     }
 }
