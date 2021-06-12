602e99791fae78b911ff6ca98d1a7a93a59c3f4a
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-06-12 17:35:04 -0400

    completed commenting out code

--- src/com/apinanyogaratnam/Utils.java
+++ src/com/apinanyogaratnam/Utils.java
@@ -4,6 +4,7 @@ import java.util.Locale;
 
 public class Utils {
     public static String removeStartEndChars(String string) {
+        // try removing start and end chars
         try {
             return string.substring(1, string.length() - 1);
         } catch (Exception e) {
@@ -11,9 +12,10 @@ public class Utils {
         }
 
         return "";
-    } // tested
+    }
 
     public static String removeEndChar(String string) {
+        // try removing end char
         try {
             return string.substring(0, string.length()-1);
         } catch (Exception e) {
@@ -25,11 +27,11 @@ public class Utils {
 
     public static String [] splitCommas(String string) {
         return string.split(",");
-    } // tested
+    }
 
     public static String [] indexList(String string) {
         return splitCommas(removeStartEndChars(string));
-    } // tested
+    }
 
     public static String parseString(String string) {
         if (!string.contains("'")) return string;
@@ -42,35 +44,44 @@ public class Utils {
             try {
                 Character d = string.charAt(i + 1);
                 if (c.equals('\'') && !d.equals('\'')) {
+                    // single quotes ex. McDonald's
                     newString += "''";
                     continue;
                 } else if (c.equals('\'') && d.equals('\'')) {
+                    // double quotes ex. McDonald''s
                     newString += "'";
                     i++;
                 }
                 newString += c;
             } catch (Exception e) {
+                // out of bounds error occurred
                 newString += c;
                 break;
             }
         }
 
         return newString;
-     } // tested
+     }
 
     public static String unParseString(String string) {
         if (!string.contains("''")) return string;
 
+        // creating a string builder object
         StringBuilder newString = new StringBuilder();
+
+        // adding string to object
         newString.append(string);
 
+        // getting index of double quotes
         int index = string.indexOf("''");
         while (index != -1) {
+            // removing a quote from double quotes
             newString.delete(index, index+1);
 
+            // updating index variable with new index to not cause an infinite loop
             index = string.indexOf("''", index+1);
         }
 
         return newString.toString();
-    } // tested
+    }
 }
