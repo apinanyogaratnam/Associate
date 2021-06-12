34e50961f790e1c7b8d1c3df7d57658dbfdb3ce8
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-06-12 15:49:49 -0400

    completed comments for mainhelper class

--- src/com/apinanyogaratnam/MainHelper.java
+++ src/com/apinanyogaratnam/MainHelper.java
@@ -4,62 +4,51 @@ import java.util.LinkedList;
 
 public class MainHelper {
     public static boolean isValidUser(String possibleUsername, LinkedList<User> allUsers) {
-        if (possibleUsername == null) return false;
-
-        possibleUsername = Utils.parseString(possibleUsername);
-        for (User user : allUsers) {
-            if (possibleUsername.equals(user.getUsername())) return true;
-        }
-
-        return false;
-    } // tested
+        return getUser(possibleUsername, allUsers) != null;
+    }
 
     public static boolean isValidCompany(String name, LinkedList<Company> allCompanies) {
-        if (name == null) return false;
-
-        name = Utils.parseString(name);
-        for (Company company : allCompanies) {
-            if (name.equals(company.getName())) return true;
-        }
-
-        return false;
-    } // tested
+        return getCompany(name, allCompanies) != null;
+    }
 
     public static int getCountOfAllUsers(LinkedList<User> allUsers) {
         return allUsers.size();
-    } // tested
+    }
 
     public static User getUser(String username, LinkedList<User> allUsers) {
         if (username == null) return null;
 
+        // searching for username
         username = Utils.parseString(username);
         for (User user : allUsers) {
             if (username.equals(user.getUsername())) return user;
         }
 
         return null;
-    } // tested
+    }
 
     public static Company getCompany(String name, LinkedList<Company> allCompanies) {
         if (name == null) return null;
 
+        // searching for name
         name = Utils.parseString(name);
         for (Company company : allCompanies) {
             if (name.equals(company.getName())) return company;
         }
 
         return null;
-    } // tested
+    }
 
     public static boolean nameInList(String friendUsername, String listOfFriendsInStringFormat) {
-        String [] strings = Utils.splitCommas(listOfFriendsInStringFormat);
-
         friendUsername = Utils.parseString(friendUsername);
         listOfFriendsInStringFormat = Utils.parseString(listOfFriendsInStringFormat);
+        String [] strings = Utils.splitCommas(listOfFriendsInStringFormat);
+
+        // searching for name in strings
         for (String string : strings) {
             if (string.equals(friendUsername)) return true;
         }
 
         return false;
-    } // tested
+    }
 }
